73a98a9b0a6279bfe7e15061420693c83d7b6b09
Neeraj Mourya <neeraj.mourya@globallogic.com>
2022-08-25 11:29:38 +0530

    final commit 25/8

--- src/test/java/com/marketo/qa/Tests/test.java
+++ src/test/java/com/marketo/qa/Tests/test.java
@@ -1,17 +1,18 @@
 package com.marketo.qa.Tests;
 
-import com.marketo.qa.base.TestBase;
-import com.marketo.qa.utility.reports;
 
 
-public class test extends TestBase {
+
+
+
+
+
+
+public class test  {
+	
+	 
+	
+
 	
 	
-	
-	public static void main(String Args[]) throws Exception {
-	
-		reports.docs();
-	
-		
-	}
 }
