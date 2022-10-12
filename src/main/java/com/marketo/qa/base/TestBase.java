171e1403803ff03130790d66652c5c2c98b0c111
Pradyumna Sahoo <pradyumna.sahoo@globallogic.com>
2022-10-12 05:10:25 +0000

    git ignore update

--- src/main/java/com/marketo/qa/base/TestBase.java
+++ src/main/java/com/marketo/qa/base/TestBase.java
@@ -78,9 +78,9 @@ public class TestBase {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.get(prop.getProperty("ghosturl"));
+		driver.get(prop.getProperty("url"));
 		new GhostLoginPage().verifyLoginPage();
-		GhostLogin();
+		Login();
 	}
 
 	public static void OpenSupportTool() {
